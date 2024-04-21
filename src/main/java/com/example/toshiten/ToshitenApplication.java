8bbdaade144ab33727003f3681afa9d0874f245f
ita-jp <28795978+ita-jp@users.noreply.github.com>
2024-04-21 21:40:58 +0900

    first commit

---
+++ src/main/java/com/example/toshiten/ToshitenApplication.java
@@ -0,0 +1,13 @@
+package com.example.toshiten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToshitenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToshitenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/toshiten/ToshitenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toshiten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToshitenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
